510cfce8a29197c87ecca003e1e7cc89ec4889c8
Willian Silvestre <silvestrewillian073@gmail.com>
2025-05-20 00:27:28 -0300

    1.2

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -16,9 +16,9 @@ function Experience() {
       },
       {
         company: "MZZI Think Digital",
-        role: "Desenvolvedor de Sistemas Web Freelancer",
+        role: "Desenvolvedor de Sistemas Web",
         period: "Julho/2023 - Janeiro/2025",
-        tasks: "Construção e manutenção de sistemas web, incluindo reuniões para direcionamento e teste de usabilidade.",
+        tasks: "Desenvolvedor contratado por projeto, para construção e manutenção de sistemas web, incluindo reuniões para direcionamento e teste de usabilidade.",
         image: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       },
     ];
